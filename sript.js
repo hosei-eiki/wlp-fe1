2e411e630fd7d682fe9dbbb788af9c2e6a4274d6
hosei-eiki <148673955+hosei-eiki@users.noreply.github.com>
2023-11-13 19:51:31 +0900

    Create sript.js

---
+++ sript.js
@@ -0,0 +1,25 @@
+function postData() {
+    // テキストボックスの値を取得
+    const textInputValue = document.getElementById('textInput').value;
+
+    // JSON形式に変換
+    const data = {
+        text: textInputValue
+    };
+
+    // Fetch APIを使用してPOSTリクエストを送信
+    fetch('https://4pev16rhda.execute-api.ap-southeast-2.amazonaws.com/WordLearningProject/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Success:', data);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
